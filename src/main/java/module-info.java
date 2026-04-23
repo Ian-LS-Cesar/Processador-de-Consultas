7de9bd27089d2c51c03a99105e0ca063881c80b8
Pedro Santos <pedroguilherme.pessoal@gmail.com>
2026-04-23 20:31:46 -0300

    feature: initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,12 @@ module com.grupo8.processadorconsultas {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Essencial para o JavaFX conseguir instanciar sua classe MainApp
+    opens com.grupo8.processadorconsultas.view to javafx.graphics;
 
+    // Caso você use FXML no futuro
     opens com.grupo8.processadorconsultas to javafx.fxml;
+
     exports com.grupo8.processadorconsultas;
+    exports com.grupo8.processadorconsultas.view;
 }
